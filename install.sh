963082e3844571bcbb0166b4fad0cee02a5b03c7
Dane Elwell <dane.elwell@gmail.com>
2018-08-27 08:54:27 +0100

    Make curl silent

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ fi
 
 cp dotvimrc "$HOME/.vimrc"
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 vim +PlugInstall +qall
